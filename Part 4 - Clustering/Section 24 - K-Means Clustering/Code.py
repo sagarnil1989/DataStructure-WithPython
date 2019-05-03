60ff49ba75d0c351189d46217f6d43bafb41cd80
sagarnil dasgupta <sagarnil1989@gmail.com>
2019-05-03 08:58:45 +0200

    Updaing K nearest Clustering Template

--- Part 4 - Clustering/Section 24 - K-Means Clustering/Code.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/Code.py
@@ -9,5 +9,31 @@ dataset=pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:,[3,4]].values
 
 from sklearn.cluster import KMeans
-for i in range(1,11):
-    kmeans=KMeans(n_cluster=i, init='k-means++',max_tier=300,n_init= 10, random_state= 0)
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#Applying to dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 0)
+y_means=kmeans.fit_predict(X)
+
+#Visualising the Cluster
+plt.close()
+plt.scatter(X[y_means==0,0],X[y_means==0,1],s=100,c='red',label='Cluster 1')
+plt.scatter(X[y_means==1,0],X[y_means==1,1],s=100,c='blue',label='Cluster 2')
+plt.scatter(X[y_means==2,0],X[y_means==2,1],s=100,c='green',label='Cluster 3')
+plt.scatter(X[y_means==3,0],X[y_means==3,1],s=100,c='brown',label='Cluster 4')
+plt.scatter(X[y_means==4,0],X[y_means==4,1],s=100,c='pink',label='Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,c='yellow',label='Centroids')
+plt.title('Cluster of Clients')
+plt.xlabel('Annual Income(k$')
+plt.ylabel('Spending score(1-100)')
+plt.legend()
+plt.show()
---
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/Code.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#importing the mall datset with pandas
+dataset = pd.read_csv('Mall_Customers.csv')
+
+
+
