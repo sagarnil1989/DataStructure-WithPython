4d0d4982fc1a389205e2f3e1b14aa89e4647e529
sagarnil dasgupta <sagarnil1989@gmail.com>
2019-05-05 10:39:51 +0200

    Updated code of Hierarchical Clsutering

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/Code.py
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/Code.py
@@ -5,6 +5,15 @@ import numpy as np
 
 #importing the mall datset with pandas
 dataset = pd.read_csv('Mall_Customers.csv')
+X= dataset.iloc[:,[3,4]].values
+
+#using dendogram to find optimal number if clusters
+import scipy.cluster.hierarchy import sch
+dendrogram=sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distance')
+plt.show()
 
 
 
