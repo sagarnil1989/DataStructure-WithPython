2f6dc44c28f4743a1e18388838c9404ac8ae5aa7
sagarnil dasgupta <sagarnil1989@gmail.com>
2019-05-07 09:08:05 +0200

    adding hierarchicak clustering and NLP

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/Code.py
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/Code.py
@@ -8,12 +8,27 @@ dataset = pd.read_csv('Mall_Customers.csv')
 X= dataset.iloc[:,[3,4]].values
 
 #using dendogram to find optimal number if clusters
-import scipy.cluster.hierarchy import sch
+import scipy.cluster.hierarchy as sch
 dendrogram=sch.dendrogram(sch.linkage(X, method='ward'))
 plt.title('Dendrogram')
 plt.xlabel('Customers')
 plt.ylabel('Euclidean Distance')
 plt.show()
 
+#Fitting Hierarchical clustering to the mall dataset
+from sklearn.cluster import AgglomerativeClustering
+hc=AgglomerativeClustering(n_clusters=5, affinity='euclidean',linkage='ward')
+y_hc=hc.fit_predict(X)
 
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
 
