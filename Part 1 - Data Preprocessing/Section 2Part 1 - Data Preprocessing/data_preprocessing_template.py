a525426b827e6a21d79cbe2fbcc16cc5d9a9c1a5
sagarnil dasgupta <sagarnil1989@gmail.com>
2019-05-22 08:55:24 +0200

    flixbus challenge solving

--- Part 1 - Data Preprocessing/Section 2Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -6,16 +6,93 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('marketing_dataset.csv',sep=';')
+dataset.describe()
+dataset.isnull().sum()
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+dataset.head()
+dataset.dtypes
+dataset_encoded=pd.get_dummies(dataset)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
+X= dataset_encoded
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#Choosing 3 cluster
+#Applying to dataset
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 0)
+y_means = kmeans.fit_predict(X)
+plt.close()
+#Visualising the Cluster
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2).fit(X)
+pca_2d = pca.transform(X)
+import pylab as plt
+for i in range(0, pca_2d.shape[0]):
+    if y_means[i] == 0:
+        c1 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='r',marker='+')
+    elif y_means[i] == 1:
+        c2 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='g',marker='o')
+    elif y_means[i] == 2:
+        c3 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='b',marker='*')
+plt.legend([c1, c2, c3], ['C1', 'C2','C3'])
+plt.title('Marketing dataset with 3 clusters and known outcomes')
+plt.show()
+
+#Choosing 4 cluster
+#Applying to dataset
+kmeans = KMeans(n_clusters = 4, init = 'k-means++', random_state = 0)
+y_means = kmeans.fit_predict(X)
+plt.close()
+#Visualising the Cluster
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2).fit(X)
+pca_2d = pca.transform(X)
+import pylab as plt
+for i in range(0, pca_2d.shape[0]):
+    if y_means[i] == 0:
+        c1 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='r',marker='+')
+    elif y_means[i] == 1:
+        c2 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='g',marker='o')
+    elif y_means[i] == 2:
+        c3 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='b',marker='*')
+    elif y_means[i] == 3:
+        c4 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='y',marker='^')
+plt.legend([c1, c2, c3, c4], ['C1', 'C2','C3','C4'])
+plt.title('Marketing dataset with 4 clusters and known outcomes')
+plt.show()
+
+#Choosing 5 cluster
+#Applying to dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 0)
+y_means = kmeans.fit_predict(X)
+plt.close()
+#Visualising the Cluster
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2).fit(X)
+pca_2d = pca.transform(X)
+import pylab as plt
+for i in range(0, pca_2d.shape[0]):
+    if y_means[i] == 0:
+        c1 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='r',marker='+')
+    elif y_means[i] == 1:
+        c2 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='g',marker='o')
+    elif y_means[i] == 2:
+        c3 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='b',marker='*')
+    elif y_means[i] == 3:
+        c4 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='y',marker='^')
+    elif y_means[i] == 4:
+        c5 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='grey',marker='>')
+plt.legend([c1, c2, c3, c4, c5], ['C1', 'C2','C3','C4','C5'])
+plt.title('Marketing dataset with 5 clusters and known outcomes')
+plt.show()
+
